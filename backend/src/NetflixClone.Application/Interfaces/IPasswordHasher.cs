9ce35432ed6f6c6347175ef660256c1d688d333f
Mevlüt Kural <sam.aldrsn@protonmail.com>
2025-01-11 22:11:03 +0300

    feat: add CreateUser command with CQRS pattern

---
+++ backend/src/NetflixClone.Application/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+namespace NetflixClone.Application.Interfaces;
+
+public interface IPasswordHasher
+{
+    string HashPassword(string password);
+    bool VerifyPassword(string password, string passwordHash);
+} 
